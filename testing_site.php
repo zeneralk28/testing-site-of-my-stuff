d64117366879d5a25ca25141d977aa90e504a84b
zeneralk28 <masuelaglen@gmail.com>
2024-09-11 01:27:56 +0800

    heheheheh

---
+++ testing_site.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Showing how php functions result:</h1>
+
+<h2>file_get_contents</h2>
+
+<h2>file_put_contents()</h2>
+
+<h2>file_exists()</h2>
+<?php print_r(file("testing4.txt")); ?>
+<h2>file()</h2>
+
+</body>
+</html>
